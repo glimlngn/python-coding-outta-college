f3644bdbf1c1ea0f0f83e8c23516e2e2258f82db
glimlngn <glimlingan@gmail.com>
2025-08-04 22:11:29 +0800

    feat: upskilled!

--- Day 38 - Workout Tracker/main.py
+++ Day 38 - Workout Tracker/main.py
@@ -1,16 +1,24 @@
 from dotenv import load_dotenv
 import os
 import requests
+import json
+from datetime import datetime
 
 load_dotenv()
 
-APP_ID = os.getenv("APP_ID")
-API_KEY = os.getenv("API_KEY")
+APP_ID = os.environ["APP_ID"]
+API_KEY = os.environ["API_KEY"]
+EXERCISE_NLP_ENDPOINT = os.environ["EXERCISE_NLP_ENDPOINT"]
+SHEETS_ENDPOINT = os.environ["SHEETS_ENDPOINT"]
+USER = os.environ["USER"]
+PASSWORD = os.environ["PASSWORD"]
+
 GENDER = "male"
 WEIGHT = 80
 HEIGHT = 170
 AGE = 23
-EXERCISE_NLP_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+DATE_NOW = datetime.now().strftime("%m/%d/%Y")
+TIME_NOW = datetime.now().strftime("%X")
 
 headers = {
     "x-app-id": APP_ID,
@@ -19,7 +27,7 @@ headers = {
 
 query = input("What exercises did you do today? ")
 
-params = {
+exercise_nlp_params = {
     "gender": GENDER,
     "weight_kg": WEIGHT,
     "height_cm": HEIGHT,
@@ -27,5 +35,22 @@ params = {
     "query": query
 }
 
-response = requests.post(url=EXERCISE_NLP_ENDPOINT, headers=headers, json=params)
-print(response.text)
+exercise_nlp_response = requests.post(url=EXERCISE_NLP_ENDPOINT, json=exercise_nlp_params, headers=headers)
+exercise = exercise_nlp_response.json()["exercises"][0]["name"]
+duration = exercise_nlp_response.json()["exercises"][0]["duration_min"]
+calories = exercise_nlp_response.json()["exercises"][0]["nf_calories"]
+# print(json.dumps(exercise_nlp_response.json(), indent=4))
+
+sheets_nlp_params = {
+    "workout": {
+        "date": DATE_NOW,
+        "time": TIME_NOW,
+        "exercise": exercise.title(),
+        "duration": duration,
+        "calories": calories
+  }
+}
+
+print(json.dumps(sheets_nlp_params, indent=4))
+sheet_response = requests.post(url=SHEETS_ENDPOINT, json=sheets_nlp_params, auth=(USER, PASSWORD))
+# print(sheet_response)
