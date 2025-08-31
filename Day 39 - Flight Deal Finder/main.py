dd38fcab0104c6eb1a050ef4a1dc4ff0675161b0
glimlngn <124807171+glimlngn@users.noreply.github.com>
2025-08-31 15:36:21 +0800

    Delete Day 39 - Flight Deal Finder directory

--- Day 39 - Flight Deal Finder/main.py
+++
@@ -1,5 +0,0 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
