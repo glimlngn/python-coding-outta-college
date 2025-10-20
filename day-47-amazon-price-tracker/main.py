281eb0bbe7ca3946e5224856e3526e987c654a9a
glimlngn <glimlingan@gmail.com>
2025-10-20 11:36:23 +0800

    fix: change method from beautiful soup to selenium

--- day-47-amazon-price-tracker/main.py
+++ day-47-amazon-price-tracker/main.py
@@ -1,48 +1,43 @@
-import requests
 import smtplib
-from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 import os
-import ast
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 load_dotenv()
 
 EMAIL_ADDRESS = os.environ["EMAIL_ADDRESS"]
 EMAIL_PASSWORD = os.environ["EMAIL_PASSWORD"]
-USER_AGENT = os.environ["USER_AGENT"]
-ACCEPT_LANGUAGE = os.environ["ACCEPT_LANGUAGE"]
-HEADERS = ast.literal_eval(os.environ['HEADERS'])
-
 AMAZON_URL = 'https://www.amazon.com/dp/B075CYMYK6?th=1'
 
 TARGET_PRICE = 120.00
 
-# Web scraping does not work for dynamic web pages that render the final HTML file using JavaScript
-# TODO: Change method from Beautiful Soup to Selenium to handle dynamic web page
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option('detach', True)
 
-response = requests.get(AMAZON_URL, headers=HEADERS)
-soup = BeautifulSoup(response.content, 'html.parser')
+driver = webdriver.Chrome(options=chrome_options)
+driver.get(AMAZON_URL)
 
+response = driver.page_source
 with open('./day-47-amazon-price-tracker/output.txt', mode='w', encoding='utf-8') as file:
-    file.write(response.text)
+    file.write(response)
 
-product_name = soup.find(name='span', id='productTitle').getText().split() # type: ignore
-product_name = ' '.join(product_name)
-print(product_name)
-price_symbol = soup.find(name='span', class_='a-price-symbol').getText() # type: ignore
-price_whole = soup.find(name='span', class_='a-price-whole').getText() # type: ignore
-price_fraction = soup.find(name='span', class_='a-price-fraction').getText() # type: ignore
+product_name = driver.find_element(By.ID, value='productTitle').text
+product_price_symbol = driver.find_element(By.CLASS_NAME, value='a-price-symbol').text
+product_price_whole = driver.find_element(By.CLASS_NAME, value='a-price-whole').text
+product_price_fraction = driver.find_element(By.CLASS_NAME, value='a-price-fraction').text
+product_price = float(f'{product_price_whole}.{product_price_fraction}')
 
-price_value = float(f'{price_whole}{price_fraction}')
-print(price_value)
-
-if price_value < TARGET_PRICE:
+if product_price < TARGET_PRICE:
     with smtplib.SMTP(host="smtp.gmail.com", port=587) as connection:
         connection.starttls()
         connection.login(user=EMAIL_ADDRESS, password=EMAIL_PASSWORD)
         connection.sendmail(
             from_addr=EMAIL_ADDRESS, 
             to_addrs=EMAIL_ADDRESS, 
-            msg=f"Subject:Low price alert on flight deals!\
-                  \n\n{product_name}\n{price_symbol}{price_value}\n{AMAZON_URL}".encode('utf-8')
+            msg=f"Subject:Low price alert on item!\
+                  \n\n{product_name}\n{product_price_symbol}{product_price}\n{AMAZON_URL}".encode('utf-8')
         )
+    print('Email sent!')
+
+driver.quit()
